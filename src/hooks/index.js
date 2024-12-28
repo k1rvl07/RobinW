098fd0749d92ed0998d40bb479f71b6ff8ecc94d
k1rvl_ <kirkorol07@gmail.com>
2024-12-28 19:26:48 +0300

    feat: add hero

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,7 +2,7 @@ const hookModules = import.meta.glob('./*/index.js', { eager: true });
 
 const hooks = Object.keys(hookModules).reduce((acc, path) => {
   const hookName = path.replace('./', '').replace('/index.js', '');
-  acc[hookName] = hookModules[path].default || hookModules[path][hookName];
+  acc[hookName] = hookModules[path][hookName];
   return acc;
 }, {});
 
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,10 +4,16 @@ import { imgImages } from "@assets";
 import components from "@components";
 
 export const Main = () => {
-    const { Header } = components;
+    const { Header, Section, Container } = components;
     return (
         <>
             <Header />
+            <main>
+                <Section tagName="section" className="hero">
+                    <h1 className="hero__title">I’m Robin Williams. A Product Designer <span className="hero__title-span">based in Italy.</span></h1>
+                    <p className="hero__text">I’m probably the most passionate designer you will ever get to work with. If you have a great project that needs some amazing skills, I’m your guy.</p>
+                </Section>
+            </main>
         </>
     );
 };
